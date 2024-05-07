f1fa1720d77591941970bc37e5350e0c6b8df754
Ashish Upreti <160201744+asbhishupreti@users.noreply.github.com>
2024-05-07 02:41:55 +0000

    datatype

---
+++ javascript/datatypes.js
@@ -0,0 +1,22 @@
+  "use strict"; //treat all js code as newer version
+
+  // alert(3 + 3)we are using nodejs, not browser
+
+//   console.log(3 +3); console.log("Ashish");
+
+let name = "Ashish"
+let age = 24
+let pass = true/false
+
+// Number => 2 to power 53
+// bigint
+// string=> ""
+// boolearn => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// Object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
---
+++ javascript/variables.js
@@ -0,0 +1,12 @@
+const accountId = 144543
+let accountmail = "ashishupreti52@gmail.com"
+var accountpassword = "12345"
+accountcity = "morena"
+
+// accountId = 2 not allowed.
+accountmail = "fwgfwh@gmail"
+accountpassword = "00000"
+accountcity = "gwalior"
+console.log(accountId);
+
+console.table([accountId, accountmail, accountpassword, accountcity])
